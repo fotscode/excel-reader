33f8cc75ea9ca6436c99e940b2105d603ec3d3da
fots <facundotomatis@gmail.com>
2024-12-26 11:06:07 -0300

    fix(uploads): now checks if timestamp isn't undefined, also exports interface and can be used as parameter

--- src/domain/models/UploadStatus.ts
+++ src/domain/models/UploadStatus.ts
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose'
+import mongoose, { Document } from 'mongoose'
 
-interface IUploadStatus {
+interface IUploadStatus extends Document {
   uploadUUID: string;
   timestamp_enqueued: string;
   timestamp_started: string;
   timestamp_finished: string;
-  status: string;
+  status: string; // TODO: enum
   format: string;
   filename: string;
 }
@@ -22,18 +22,19 @@ const uploadStatusSchema = new mongoose.Schema<IUploadStatus>({
 
 uploadStatusSchema.pre('save', function (next) {
   const now = new Date().toISOString()
-  if (this.status === 'pending') {
+  if (this.status === 'pending' && !this.timestamp_enqueued) {
     this.timestamp_enqueued = now
   }
-  if (this.status === 'processing') {
+  if (this.status === 'processing' && !this.timestamp_started) {
     this.timestamp_started = now
   }
-  if (this.status === 'done') {
+  if (this.status === 'done' && !this.timestamp_finished) {
     this.timestamp_finished = now
   }
   next()
 })
 
-const UploadStatus = mongoose.model("UploadStatus", uploadStatusSchema)
+const UploadStatus = mongoose.model<IUploadStatus>("UploadStatus", uploadStatusSchema)
 
+export { IUploadStatus }
 export default UploadStatus
