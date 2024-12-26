ec25ff6fb168cbaa0421eede21b613f07ab25de9
fots <facundotomatis@gmail.com>
2024-12-26 11:08:48 -0300

    fix(process): replaced forEach with loops, resulting in less processing time, also made some refactorings

--- src/infrastructure/queues/consumer.ts
+++ src/infrastructure/queues/consumer.ts
@@ -1,13 +1,15 @@
 import { convertStringToJson, createSchemaFromJSON, getRowErrors, fillRowObject } from "@application/services/FormatSchema";
 import { createModelFromSchema } from "@infrastructure/repositories/DynamicFileRepo";
-import UploadStatus from "@domain/models/UploadStatus";
-import ProcessError from "@domain/models/ProcessError";
+import uploadRepo from "@infrastructure/repositories/UploadStatusRepo";
+import ProcessError, { IProcessError } from "@domain/models/ProcessError";
 import XLSX from "xlsx";
 import amqp from "amqplib";
 import mongoose from "mongoose";
+import path from "path";
 
 // TODO: change with .env
 const queueName = "csv";
+const uploadsPath = path.join(__dirname, "..", "..", "..", "uploads");
 
 // move to helper fn
 mongoose.connect('mongodb://127.0.0.1:27017/koibanx_challenge')
@@ -31,52 +33,64 @@ async function startConsumer() {
       }
       const message = JSON.parse(msg.content.toString());
       const uploadUUID = message.uploadUUID;
-      // encapsulate this as a service
-      const uploadStatus = await UploadStatus.findOne({ uploadUUID });
+      const uploadStatus = await uploadRepo.findUploadStatusByUUID(uploadUUID);
       if (!uploadStatus) {
         console.error("UploadStatus not found for uploadUUID:", uploadUUID);
+        channel.ack(msg);
         return;
       }
-      uploadStatus.status = "processing";
-      await uploadStatus.save();
+      console.log("Processing uploadUUID:", uploadUUID);
+      await uploadRepo.updateUploadStatus(uploadStatus, "processing");
       // encapsulate following
       let format = convertStringToJson(uploadStatus.format)
       let schema = createSchemaFromJSON(format);
       const DynamicModel = createModelFromSchema(schema, uploadStatus.uploadUUID);
-      const workbook = XLSX.readFile(`/home/fots/koibanx-challenge/uploads/${uploadStatus.filename}`);
+      console.log("Reading file:", `${uploadsPath}/${uploadStatus.filename}`);
+      const workbook = XLSX.readFile(`${uploadsPath}/${uploadStatus.filename}`);
       const sheetName = workbook.SheetNames[0];
       const sheet = workbook.Sheets[sheetName];
-      const data = XLSX.utils.sheet_to_json(sheet, { header: 1 }).slice(1);
+      const data = XLSX.utils.sheet_to_json(sheet, { header: 1 }).slice(1) as any[];
+      console.log("Finished reading file:", `${uploadsPath}/${uploadStatus.filename}`);
       let rows = [] as typeof DynamicModel[];
-      data.forEach((row: any, index) => {
+      let errors = [] as IProcessError[];
+      let index = 0;
+      const BATCH_SIZE = 256;  // config
+      console.log("Processing data with BATCH_SIZE:", BATCH_SIZE);
+      for (let row of data) {
         let obj = new DynamicModel()
         fillRowObject(obj, row, schema)
         let error = obj.validateSync();
         if (error) {
           let rowErrors = getRowErrors(index + 1, error.errors, schema)
-          rowErrors.forEach((rowError) => {
+          for (let rowError of rowErrors) {
             let processError = new ProcessError({
               _upload: uploadStatus._id,
               uploadUUID: uploadStatus.uploadUUID,
               row: rowError.row,
               col: rowError.col,
             })
-            processError.save()
-          })
+            errors.push(processError)
+            if (errors.length === BATCH_SIZE) {
+              await ProcessError.insertMany(errors, { ordered: false })
+              errors = []
+            }
+          }
         } else {
           rows.push(obj)
-          if (rows.length === 8192) { // config
-            DynamicModel.insertMany(rows, { ordered: false })
+          if (rows.length === BATCH_SIZE) {
+            await DynamicModel.insertMany(rows, { ordered: false })
             rows = [];
           }
         }
-      });
+        index++;
+      }
       if (rows.length > 0) {
         await DynamicModel.insertMany(rows, { ordered: false })
       }
-      uploadStatus.status = "done"
-      uploadStatus.save()
-
+      if (errors.length > 0) {
+        await ProcessError.insertMany(errors, { ordered: false })
+      }
+      await uploadRepo.updateUploadStatus(uploadStatus, "done");
       channel.ack(msg);
     },
     { noAck: false }
