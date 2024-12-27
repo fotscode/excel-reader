68f95e7752c31f76174536131fb5492754013de0
fots <facundotomatis@gmail.com>
2024-12-26 23:11:22 -0300

    feat(errors): added errors interface and distinct codes

---
+++ src/interface/mappers/error.ts
@@ -0,0 +1,61 @@
+import { Response } from 'express';
+
+enum ECODES {
+    FILE_NOT_FOUND = 'FILE_NOT_FOUND',
+    UPLOAD_STATUS_NOT_FOUND = 'UPLOAD_STATUS_NOT_FOUND',
+    UPLOAD_UUID_NOT_PROVIDED = 'UPLOAD_UUID_NOT_PROVIDED',
+    PAGE_MUST_BE_GREATER_THAN_ZERO = 'PAGE_MUST_BE_GREATER_THAN_ZERO',
+    LIMIT_MUST_BE_GREATER_THAN_ZERO = 'LIMIT_MUST_BE_GREATER_THAN_ZERO',
+}
+
+type ErrorKeys = keyof typeof ECODES;
+
+interface IError {
+    identifier: ECODES;
+    message: string;
+    httpCode: number,
+    timestamp?: string,
+    extra?: any
+}
+
+const ERRORS: Record<ErrorKeys, IError> = {
+    FILE_NOT_FOUND: {
+        identifier: ECODES.FILE_NOT_FOUND,
+        message: 'File not found',
+        httpCode: 404,
+    },
+    UPLOAD_STATUS_NOT_FOUND: {
+        identifier: ECODES.UPLOAD_STATUS_NOT_FOUND,
+        message: 'UploadStatus not found',
+        httpCode: 404,
+    },
+    UPLOAD_UUID_NOT_PROVIDED: {
+        identifier: ECODES.UPLOAD_UUID_NOT_PROVIDED,
+        message: 'Upload UUID not provided',
+        httpCode: 400,
+    },
+    PAGE_MUST_BE_GREATER_THAN_ZERO: {
+        identifier: ECODES.PAGE_MUST_BE_GREATER_THAN_ZERO,
+        message: 'Page must be greater than zero',
+        httpCode: 400,
+    },
+    LIMIT_MUST_BE_GREATER_THAN_ZERO: {
+        identifier: ECODES.LIMIT_MUST_BE_GREATER_THAN_ZERO,
+        message: 'Limit must be greater than zero',
+        httpCode: 400,
+    },
+}
+
+const findError = (identifier: ECODES, extra: any = undefined): IError => {
+    const err = ERRORS[identifier];
+    err.timestamp = new Date().toISOString();
+    if (extra) err.extra = { extra };
+    return err
+}
+
+const makeHTTPError = (res: Response, identifier: ECODES, extra: any = undefined) => {
+    const err = findError(identifier, extra);
+    res.status(err.httpCode).json(err);
+}
+
+export { IError, findError, ECODES, makeHTTPError }
