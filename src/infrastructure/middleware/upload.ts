65c4cbdea98be5efd60234a10f028c628a8cb0f8
fots <facundotomatis@gmail.com>
2024-12-27 01:03:37 -0300

    refactor(upload): upload path imported from config

--- src/infrastructure/middleware/upload.ts
+++ src/infrastructure/middleware/upload.ts
@@ -1,5 +1,7 @@
 import { Request } from 'express';
 import multer from 'multer';
+import { multerUploadPath } from '@shared/config';
+
 type DestinationCallback = (error: Error | null, destination: string) => void
 type FileNameCallback = (error: Error | null, filename: string) => void
 
@@ -10,7 +12,7 @@ interface RequestWithFile extends Request {
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
   destination: (req: Request, file: any, cb: DestinationCallback) => {
-    cb(null, 'uploads/');
+    cb(null, multerUploadPath);
   },
   filename: (req: Request, file, cb: FileNameCallback) => {
     cb(null, new Date().toISOString() + '_' + file.originalname);
